78c96c2ff3985bc8b2f2171d055e7c9040f98691
Josie Newman <82321832+JosieAilsa@users.noreply.github.com>
2022-05-20 16:31:06 +0100

    routing

--- src/containers/Routes.jsx/Routing.jsx
+++ src/containers/Routes.jsx/Routing.jsx
@@ -6,7 +6,7 @@ const Routing = ({handleLiftSelect, handleWeightInput, handleDifficultyInput, ha
     return (
         <Routes>
         <Route
-          path exact="/"
+          path="/"
           element={
             <AddLift
                 handleLiftSelect={handleLiftSelect}
@@ -17,7 +17,7 @@ const Routing = ({handleLiftSelect, handleWeightInput, handleDifficultyInput, ha
           }
         />
         <Route
-          path exact= "/lifts"
+          path= "/lifts"
           element={
                 <LoggedLifts/>}
         />
